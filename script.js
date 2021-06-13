10878371168be8d5607a18cb457208b2053405ad
arvindboth <arvindboth@yahoo.co.in>
2021-06-13 08:04:03 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+function updateRate()
+{
+    var ratevalue = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=ratevalue;
+    return true;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var amount = document.getElementById("principal");
+    if (amount.value<=0)
+    {
+        alert("Enter a positive number");
+        amount.focus();
+        return false;
+    }
+    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+    resultyear = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML = 
+    "If you deposit <mark>"+principal+"</mark>,<br>&#160&#160&#160&#160 at an interest rate of <mark>"+rate+"%</mark>.<br>&#160&#160&#160&#160 You will receive an amount of <mark>"+interest+"</mark>,<br>&#160&#160&#160&#160 in the year <mark>"+resultyear+"</mark>";
+    
+    return true;
     
 }
-        
